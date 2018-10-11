8226ec624c0603dc59018b9a0e0f19f8f35287f9
shintan777 <43470116+shintan777@users.noreply.github.com>
2018-10-11 21:48:22 +0530

    the updated file
    rectangle moves only in one direction (fefefe)

--- Stac.java
+++ Stac.java
@@ -1,12 +1,28 @@
 import javax.swing.*;
 import java.awt.*;
 
+
+@SuppressWarnings("serial")
 class Stac extends JFrame
+{int x=600,y=600,w=300,h=50;
+private void moverect(double v)
 {
+if(x>0)
+x-=v;
+else 
+x+=v;	
+}
+private void nextrect()
+{
+x=600;
+y+=50;	
+}
+
+	
 public Stac()
 {
 
- super("Draw A Rectangle In JFrame");
+ super("StackItUp!!!");
 
  
 setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -21,23 +37,36 @@ setVisible(true);
  
 
 }
-
+@Override
 public void paint(Graphics g)
 {
  super.paint(g);
 
  
- g.drawRect(20,20,300,50);
+// g.drawRect(x,y,w,h);
 
  
  g.setColor(Color.RED);
 
  
- g.fillRect(50,50,300,100);
+ g.fillRect(x,y,w,h);
 }
 
-public static void main(String[]args)
+public static void main(String[]args)throws InterruptedException {
 {
  Stac rect=new Stac();
+ while(true)
+ {
+	rect.moverect(1.5);
+			rect.repaint();
+			Thread.sleep(10);
+	 
+	 
+ }
+ 
 }
+}
+
+
+
 }
