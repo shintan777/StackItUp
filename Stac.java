6ab80bf9d73ba1d7e37467cf8d0be9885cf09d1c
ADI10HERO <39651310+ADI10HERO@users.noreply.github.com>
2018-10-12 01:55:33 +0530

    Fixed the block movement problem

--- Stac.java
+++ Stac.java
@@ -1,78 +1,63 @@
+package com.mini.project;
+
 import javax.swing.*;
 import java.awt.*;
+import java.util.*;
 
-
-@SuppressWarnings("serial")
-class Stac extends JFrame
-{int x=600,y=600,w=300,h=50;
-private void moverect(double v)
-{
-double vel=-3;
-if(x<0)
-vel=v;
-else if(x>750)
-vel=(-v);
-
-x+=vel;
-
-
-
-}
-private void nextrect()
-{
-x=600;
-y+=50;	
-}
-
-	
-public Stac()
-{
-
- super("StackItUp!!!");
-
- 
-setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-setBounds(30,30,750,750);
-
-setBackground(Color.darkGray);
-setResizable(false);
-setVisible(true);
- 
- //setSize(500,500);
-
- 
-
-}
-@Override
-public void paint(Graphics g)
-{
- super.paint(g);
-
- 
-	
-// g.drawRect(x,y,w,h);
-
- 
- g.setColor(Color.RED);
-
- 
- g.fillRect(x,y,w,h);
-}
-
-public static void main(String[]args)throws InterruptedException {
-{
- Stac rect=new Stac();
- while(true)
- {
-	rect.moverect(1.5);
-			rect.repaint();
-			Thread.sleep(20);
-	 
-	 
- }
- 
-}
-}
+@SuppressWarnings({ "serial", "unused" })
+class Stac extends JFrame{
+	int x=0,y=600,w=250,h=50;
+	double vel=2.0;
+	private void moverect()
+	{	
+		if(x+vel<720 && x+vel>0)
+		{x+=vel;
+		}
+		if(x+vel>=720-w)
+		 {vel=vel*(-1);
+		  x+=vel;
+		 }
+		if(x+vel==0)
+		{vel=vel*(-1);
+		  x+=vel;
+		 }
+	}
+	private void nextrect()
+	{
+	x=600;
+	y+=50;	
+	}
+			
+		Stac()
+		{
+			super("StackItUp!!!");
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			setBounds(30,30,720,720);
+			//setSize(720,720);
+			setBackground(Color.darkGray);
+			setResizable(false);
+			setVisible(true);
+			}
+	@Override
+			public void paint(Graphics g)
+			{
+			 super.paint(g);
+			 //Graphics2D g2d = (Graphics2D) g;
+			 g.setColor(Color.RED); 
+			 g.fillRect(x,y,w,h);
+			}
+					
+		public static void main(String[]args)throws InterruptedException {
+		{
+			 Stac rect=new Stac();
+			 while(true)
+			 {
+				rect.moverect();
+				rect.repaint();
+				Thread.sleep(15);	 
+			 }
+		}
+	}
 
 
 
