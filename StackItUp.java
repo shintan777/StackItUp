1bd704c5e4cbbbfbd973abf82bbbfb220a8b861c
shintan777 <43470116+shintan777@users.noreply.github.com>
2018-10-19 02:44:40 +0530

    trying to change width

--- StackItUp.java
+++ StackItUp.java
@@ -1,34 +1,26 @@
-
 import java.awt.event.KeyEvent;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyListener;
 import java.util.*;
 
-@SuppressWarnings({ "serial", "unused" })
+@SuppressWarnings({ "serial" })
 class StackItUp extends JFrame{
-	static int cntr=0,t=0;
-	int i;
+	static int cntr=0,t=0,width=250;
+	int i,xpos,xpos2,diff;
 	JLabel  t1;
-	
-
-		Block[] b= new Block[30];
-
+	Block actual =new Block();
+	Block nextblk =new Block();
+	ArrayList<Block> b = new ArrayList<Block>();
 	
 		StackItUp()
-		{
+		{super("StackItUp!!!");
 			   
-		super("StackItUp!!!");
+		        setSize(720,720);
+				setVisible(true);
 				t1 = new JLabel() ;
-			t1.setBounds(200,100,50,30) ; 
-			
-			
-
-//ArrayList<Block> b = new ArrayList();
-    for (int i = 0; i < b.length; i++)
-            b[i] = new Block();
-    
-//actual=b.get(0);
+				t1.setBounds(0,0,100,100) ; 
+                add(t1);
 			addKeyListener(new KeyListener() {
 				
 				@Override
@@ -36,47 +28,50 @@ class StackItUp extends JFrame{
 				}
 				@Override
 				public void keyReleased(KeyEvent e) {
-					}
-				
+				}			
 				@Override
 				public void keyPressed(KeyEvent e) {
-					
-					b[t].keyPressed(e);
-					t++;
-					
-					//nextblk=b.get(t);
-					b[t].nextrect();
+					actual.keyPressed(e);
+					xpos2=actual.x;
+					//xpos2=actual.x+actual.w;
+					System.out.println(xpos2+" "+xpos);
+					//ArrayList<Block> b = new ArrayList();
+					b.add(new Block());
+					//System.out.println(b.size());
+					nextblk=b.get(cntr);
 					cntr++;
-				t1.setText("SCORE :  "+cntr);
-					
-				
+					t1.setText("SCORE :  "+cntr);
+					nextblk.w=width;
+					if(cntr==2)
+						diff=0;
+					else
+					{diff=Math.abs(xpos-xpos2);}
+					System.out.println(diff);
+					width-=diff;
+					xpos=xpos2;
+					nextblk.y-=nextblk.h*cntr;
+					//if(cntr>=8) nextblk.drop();
+					actual=nextblk;
 				}
 			});
 			setFocusable(true);
-			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			setSize(720,720);
+		   
 			setResizable(false);
-			setVisible(true);
-			add(t1);
+			
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			}
 			
 		public void move() {
-			 for (Block ablock : b) ablock.moverect();
 			//actual.moverect();
-			//nextblk.moverect();
+			nextblk.moverect();
 			
 		}
 		@Override
 		public void paint(Graphics g) {
-		g.setColor(Color.black);
-		for (Block ablk : b) {
-            ablk.paint(g);
-        }
-			
-		//actual.paint(g);
-		//nextblk.paint(g);
-		
-			
+			super.paint(g);
+			//actual.paint(g);
+			for (Block nb : b) 
+			nb.paint(g);
 		}
 		public static void main(String[]args)throws InterruptedException {
 		{
@@ -85,25 +80,31 @@ class StackItUp extends JFrame{
 			 {
 				rect.move();
 				rect.repaint();
-				Thread.sleep(15);	 
+				Thread.sleep(20);	 
 			 }
 		}
 	}
 }
 @SuppressWarnings("serial")
- class Block extends JFrame {
+ class Block extends JFrame{
 	double vel=2.0;
-	int x=0,w=250,h=50,y=600,r=255,g=255,b=255;
+	int x=0,h=50,y=600,r=255,gn=255,b=255;
+	  int w;
 	 Block(){
-		double vel=2.0;
+		 int r = (int)(Math.random()*((255+1)));
+		 int gn = (int)(Math.random()*((255+1)));
+		 int b = (int)(Math.random()*((255+1)));
+		//double vel=2.0;
 		this.x=x;
 		this.y=y;
 		this.w=w;
 		this.h= h;
-		this.vel=vel;
-	}
- 	
-
+		this.vel=2.0;
+		this.r=r;
+		this.gn=gn;
+		this.b=b;
+	 }
+	 
 	public void moverect()
 	{	
 		if(x+vel<720 && x+vel>0)
@@ -122,22 +123,27 @@ class StackItUp extends JFrame{
 	{	
 		y-=h;
 		x=0;
-	   
 	}
 	
 	@Override
 	public void paint(Graphics g)
 	{
-	 g.setColor(Color.RED); 
+	 g.setColor(new Color(r, gn, b)); //apply random value using rand()
+		//g.setColor(Color.RED);
 	 g.fillRect(x,y,w,h);
 	}
-	
+	void drop() {
+		
+	}
 	public void keyPressed(KeyEvent e) {
 		if (e.getKeyCode() == KeyEvent.VK_DOWN || e.getKeyCode() == KeyEvent.VK_ENTER)
 		{
-			vel = 0;	
-			
-			
+			this.vel = 0.000;		
 		}
 	}
+	
+	/*public Object clone() throws CloneNotSupportedException
+	{
+		return super.clone();
+	}*/
 }
