777da63b27678ac34c7207c5012622e242bad55d
salonishedge <43499693+salonishedge@users.noreply.github.com>
2018-10-19 00:39:43 +0530

    Music will work if 9 errors are solved

--- StackItUp.java
+++ StackItUp.java
@@ -1,42 +1,30 @@
+
 import java.awt.event.KeyEvent;
 import javax.swing.*;
+
+//import sun.audio.*;
+
 import java.awt.*;
 import java.awt.event.KeyListener;
 import java.util.*;
-import java.util.Random;
+import java.io.*;
 
-@SuppressWarnings({ "serial", "unused" })
+@SuppressWarnings({ "serial" })
 class StackItUp extends JFrame{
 	static int cntr=0,t=0;
 	int i;
 	JLabel  t1;
-	Random r=new Random();
-	
-
 	Block actual =new Block();
 	Block nextblk =new Block();
-	
+	ArrayList<Block> b = new ArrayList<Block>();
 	
 		StackItUp()
-		{
+		{super("StackItUp!!!");
 			   
-		super("StackItUp!!!");
+		
 				t1 = new JLabel() ;
-			t1.setBounds(200,100,50,30) ; 
-			
-			
+				t1.setLocation(0,0) ; 
 
-ArrayList<Block> b = new ArrayList();
-for( i=0;i<30;i++)
-{
-	Block blk= new Block();
-	blk.y-=blk.h;
-	blk.x=0;
-	
-	b.add(blk);
-	
-}
-//actual=b.get(0);
 			addKeyListener(new KeyListener() {
 				
 				@Override
@@ -44,66 +32,43 @@ for( i=0;i<30;i++)
 				}
 				@Override
 				public void keyReleased(KeyEvent e) {
-					}
-				
+				}			
 				@Override
 				public void keyPressed(KeyEvent e) {
-					
 					actual.keyPressed(e);
+					//ArrayList<Block> b = new ArrayList();
+					b.add(new Block());
+					System.out.println(b.size());
+					nextblk = b.get(cntr);
 					cntr++;
-				nextblk=b.get(t++);
-				
-				//nextblk.nextrect();
-				t1.setText("SCORE :  "+cntr);
-					
-				
+					t1.setText("SCORE :  "+cntr);
+					//nextblk.nextrect();
+					//nextblk.keyPressed(e);
+					nextblk.y-=nextblk.h*cntr;
+					//if(cntr>=8) nextblk.drop();
+					actual=nextblk;
+					//sound();
 				}
 			});
 			setFocusable(true);
 			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			setSize(720,720);
-			
-			//setBackground(Color.red);
-			
 			setResizable(false);
 			setVisible(true);
 			add(t1);
-		for(int i=0;i<30;i++)
-		{
-			Block[i]=new Block(i,r.nextInt(255),r.nextInt(255),r.nextInt(255));
-		}
-
-			
 			}
 			
-		public void updateColors(){
-		for(int i=0;i<30;i++)
-		{
-			Block[i].setColor(r.nextInt(255),r.nextInt(255),r.nextInt(255));
-		}	
-		}
-		
-	    public void actionPerformed(ActionEvent arg0)
-		{
-			updateColors();
-		}
-			
 		public void move() {
-			actual.moverect();
+			//actual.moverect();
 			nextblk.moverect();
 			
 		}
 		@Override
 		public void paint(Graphics g) {
 			super.paint(g);
-			actual.paint(g);
-		nextblk.paint(g);
-		for(int i=0;i<30;i++)
-		{
-			Block[i].draw(g);
-		}
-		
-			
+			//actual.paint(g);
+			for (Block nb : b) 
+			nb.paint(g);
 		}
 		public static void main(String[]args)throws InterruptedException {
 		{
@@ -117,23 +82,69 @@ for( i=0;i<30;i++)
 		}
 	}
 }
+/*class Sound
+{
+	
+public static class AL implements ActionListener{
+	public final void actionPerformed(ActionEvent e)
+	{
+		sound();
+	}
+}
+	
+public static void sound(){
+	
+	AudioPlayer MGP = AudioPlayer.player;
+	AudioStream BGM;
+	AudioData MD;
+ContinousAudioDataStream loop=null;
+
+try{
+BGM =new AudioStream(new FileInputStream("Sound.wav"));
+MD =BGM.getData();
+loop= new ContinousAudioDataStream(MD);
+}catch(IOException error){}
+	
+	MGP.start(loop);
+}
+
+}*/
+public static synchronized void playSound(final String url) {
+  new Thread(new Runnable() {
+  // The wrapper thread is unnecessary, unless it blocks on the
+  // Clip finishing; see comments.
+    public void run() {
+      try {
+        Clip clip = AudioSystem.getClip();
+        AudioInputStream inputStream = AudioSystem.getAudioInputStream(
+          Main.class.getResourceAsStream("Sound.wav" + url));
+        clip.open(inputStream);
+        clip.start(); 
+      } catch (Exception e) {
+        System.err.println(e.getMessage());
+      }
+    }
+  }).start();
+}
 @SuppressWarnings("serial")
- class Block extends JFrame {
+ class Block extends JFrame{
 	double vel=2.0;
-	int x=0,w=250,h=50,y=600,R,G,B;
-	 Block(int R,int G,int B){
-		double vel=2.0;
+	int x=0,w=250,h=50,y=600,r=255,gn=255,b=255;
+	 Block(){
+		 int r = (int)(Math.random()*((255+1)));
+		 int gn = (int)(Math.random()*((255+1)));
+		 int b = (int)(Math.random()*((255+1)));
+		//double vel=2.0;
 		this.x=x;
 		this.y=y;
 		this.w=w;
 		this.h= h;
-		this.vel=vel;
-		this.R=R;
-		this.G=G;
-		this.B=B;
-	}
- 	
-
+		this.vel=2.0;
+		this.r=r;
+		this.gn=gn;
+		this.b=b;
+	 }
+	 
 	public void moverect()
 	{	
 		if(x+vel<720 && x+vel>0)
@@ -152,26 +163,27 @@ for( i=0;i<30;i++)
 	{	
 		y-=h;
 		x=0;
-	   
 	}
 	
 	@Override
 	public void paint(Graphics g)
 	{
-	 g.setColor(new Color(R,G.B)); 
+	 g.setColor(new Color(r, gn, b)); //apply random value using rand()
+		//g.setColor(Color.RED);
 	 g.fillRect(x,y,w,h);
 	}
-	
-	public void setColor(int R,int G,int B)
-	{
-		this.R=R;
-		this.G=G;
-		this.B=B;
+	void drop() {
+		
 	}
 	public void keyPressed(KeyEvent e) {
 		if (e.getKeyCode() == KeyEvent.VK_DOWN || e.getKeyCode() == KeyEvent.VK_ENTER)
 		{
-			vel = 0;		
+			this.vel = 0.000;		
 		}
 	}
+	
+	/*public Object clone() throws CloneNotSupportedException
+	{
+		return super.clone();
+	}*/
 }
