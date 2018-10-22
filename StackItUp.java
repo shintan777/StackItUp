53a33a9ba70d4033faf0c0c926b8fec0f852f59f
ADI10HERO <39651310+ADI10HERO@users.noreply.github.com>
2018-10-22 07:18:02 +0530

    Latest code all working except sound and cutting

--- StackItUp.java
+++ StackItUp.java
@@ -1,5 +1,4 @@
 import java.awt.event.KeyEvent;
-
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.*;
@@ -7,19 +6,29 @@ import java.io.*;
 import java.awt.*;
 import java.awt.event.KeyListener;
 import java.util.*;
+import javax.swing.JPanel;
 
 @SuppressWarnings({ "serial" })
 class StackItUp extends JFrame{
 	static int cntr=0,t=0;
-	int i,xpos,xpos2,diff,h1;
-	JLabel  t1 = new JLabel();
+	int i,x1,x2,diff,xe1,xe2,width=250,yc=650;
 	Block nextblk =new Block();
+	Block temp =new Block();
 	ArrayList<Block> b = new ArrayList<Block>();
 	
 	StackItUp()
-	{super("StackItUp!!!");
-
+	{
+    super("StackItUp");
 	
+	JPanel newPanel = new JPanel();
+	newPanel.setBackground(Color.BLACK);
+	JLabel  t1 = new JLabel();
+	t1.setBounds(10,10,50,50);
+	t1.setForeground(Color.WHITE);
+		newPanel.add(t1);
+		add(newPanel);
+		
+       
 			addKeyListener(new KeyListener() {
 			@Override
 			public void keyTyped(KeyEvent e) {
@@ -32,33 +41,32 @@ class StackItUp extends JFrame{
 				if(cntr<10)		
 				{	
 					b.add(new Block());
-				nextblk = b.get(cntr);
-				xpos2=nextblk.x;
-				cntr++;
-				t1.setText("SCORE :  "+cntr);//score needs to be corrected
-				nextblk.y-=nextblk.h*cntr;
+					nextblk = b.get(cntr);
+					nextblk.w=width;
+					//xpos2=nextblk.x;
+					cntr++;
+					t1.setText("SCORE :  "+cntr);//score needs to be corrected
+					nextblk.y-=nextblk.h*cntr;
+				
 				}
 				else {
 					
 					b.add(new Block());
 					Block temp = b.get(b.size()-1);
-					//temp.y+=temp.h;
 					nextblk =temp;
 					t1.setText("SCORE :  "+(cntr-1));//score needs to be corrected
-					//nextblk.y-=nextblk.h*cntr;
 					nextblk.y-=550;
+					nextblk.w=width;
 					cntr++;
 					drop();
 				}
 			}
 		});
-			t1.setLocation(0,-250) ;
-			setFocusable(true);
+			
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(720,720);
-		setResizable(false);
-		setVisible(true);
-		add(t1);
+	    setResizable(false);
+	   	setVisible(true);
 		}
 			
 		public void move() {
@@ -73,7 +81,6 @@ class StackItUp extends JFrame{
 		void drop()
 		{ 	for (Block nb : b)
 				{nb.y+=nb.h;
-				
 				}
 		}
 		
@@ -95,13 +102,14 @@ class StackItUp extends JFrame{
 		
 		
 		public static void main(String[]args)throws InterruptedException {
-		{
-			 StackItUp rect=new StackItUp();
+		{  
+	    
+		StackItUp rect=new StackItUp(); 
 			 while(true)
 			 {
 				rect.move();
 				rect.repaint();
-				Thread.sleep(10);	 
+				Thread.sleep(15);	 
 			 }
 		}
 	}
@@ -109,17 +117,16 @@ class StackItUp extends JFrame{
 @SuppressWarnings("serial")
  class Block extends JFrame{
 	double vel=2.0;
-	int x=0,w=250,h=50,y=650,r=255,gn=255,b=255;
+	int x,w,h=50,y=650,r=255,gn=255,b=255;
 	 Block(){
 		 int r = (int)(Math.random()*((255+1)));
 		 int gn = (int)(Math.random()*((255+1)));
 		 int b = (int)(Math.random()*((255+1)));
-		//double vel=2.0;
 		this.x=x;
 		this.y=y;
 		this.w=w;
 		this.h= h;
-		this.vel=2.0;
+		this.vel=vel;
 		this.r=r;
 		this.gn=gn;
 		this.b=b;
@@ -148,8 +155,7 @@ class StackItUp extends JFrame{
 	@Override
 	public void paint(Graphics g)
 	{
-	 g.setColor(new Color(r, gn, b)); //apply random value using rand()
-		//g.setColor(Color.RED);
+	 g.setColor(new Color(r, gn, b));
 	 g.fillRect(x,y,w,h);
 	}
 	public void keyPressed(KeyEvent e) {
