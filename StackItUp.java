6ecdee25579788d57f0d6d17a6b71237ed5f1ad7
salonishedge <43499693+salonishedge@users.noreply.github.com>
2018-10-22 23:48:28 +0530

    with background

--- StackItUp.java
+++ StackItUp.java
@@ -1,109 +1,144 @@
 import java.awt.event.KeyEvent;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 import javax.swing.*;
+import java.io.*;
 import java.awt.*;
 import java.awt.event.KeyListener;
 import java.util.*;
-import java.io.*;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
+import javax.swing.JPanel;
 
 @SuppressWarnings({ "serial" })
 class StackItUp extends JFrame{
 	static int cntr=0,t=0;
-	int i;
-	JLabel  t1;
+	int i,x1,x2,diff,xe1,xe2,width=250,yc=650;
 	Block nextblk =new Block();
+	Block temp =new Block();
 	ArrayList<Block> b = new ArrayList<Block>();
 	
-		StackItUp()
-		{super("StackItUp!!!");
-			   
-		
-				t1 = new JLabel() ;
-				t1.setLocation(0,0) ; 
 
-				addKeyListener(new KeyListener() {
-				
-				@Override
-				public void keyTyped(KeyEvent e) {
-				}
-				@Override
-				public void keyReleased(KeyEvent e) {
-				}			
-				@Override
-				public void keyPressed(KeyEvent e) {
-					b.add(new Block());
-					System.out.println(b.size());
-					nextblk = b.get(cntr);
-					cntr++;
-					t1.setText("SCORE :  "+cntr);
-					nextblk.y-=nextblk.h*cntr;
-					//if(cntr>=8) nextblk.drop();
-					//actual=nextblk;
-					//sound();
-				}
-			});
-			setFocusable(true);
-			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			setSize(720,720);
-			setResizable(false);
-			setVisible(true);
-			add(t1);
+	StackItUp()
+	{
+    super("StackItUp");
+	JLabel background;
+	ImageIcon img = new ImageIcon("sal.jpg");
+	background = new JLabel("",img,JLabel.CENTER);
+	background.setBounds(0,0,720,720);
+	add(background);
+	JPanel newPanel = new JPanel();
+	newPanel.setBackground(Color.BLACK);
+	JLabel  t1 = new JLabel();
+	t1.setBounds(10,10,50,50);
+	t1.setForeground(Color.BLACK);
+		newPanel.add(t1);
+		add(newPanel);
+		
+
+		File bg =  new File(".//bg.wav");
+		try {
+			AudioInputStream ais = AudioSystem.getAudioInputStream(bg);
+			Clip clip = AudioSystem.getClip();
+			clip.open(ais);
+			clip.start();
+			
+		
+		
+		} catch(Exception e) {System.out.println(e);}
+
+       
+			addKeyListener(new KeyListener() {
+			@Override
+			public void keyTyped(KeyEvent e) {
 			}
+			@Override
+			public void keyReleased(KeyEvent e) {
+			}			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if(cntr<10)		
+				{	
+					b.add(new Block());
+					nextblk = b.get(cntr);
+					nextblk.w=width;
+					//xpos2=nextblk.x;
+					cntr++;
+					t1.setText("SCORE :  "+(cntr-1));//score needs to be corrected
+
+					nextblk.y-=nextblk.h*cntr;
+				
+				}
+				else {
+					
+					b.add(new Block());
+					Block temp = b.get(b.size()-1);
+
+					nextblk =temp;
+					t1.setText("SCORE :  "+(cntr-1));//score needs to be corrected
+					nextblk.y-=550;
+					nextblk.w=width;
+					cntr++;
+					drop();
+				}
+			}
+		});
+			
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(720,720);
+	    setResizable(false);
+	   	setVisible(true);
+
+		}
 			
 		public void move() {
 			nextblk.moverect();
-			
 		}
-		@Override
+		
 		public void paint(Graphics g) {
 			super.paint(g);
 			for (Block nb : b) 
-			nb.paint(g);
+				nb.paint(g);
 		}
-		public static void main(String[]args)throws InterruptedException 
-		{
-			 StackItUp rect=new StackItUp();
-			 File abc = new File("bg.wav"); 
-	         PlaySound(abc);
+		void drop()
+		{ 	for (Block nb : b)
+				{nb.y+=nb.h;
+				}
+
+		}	
+		
+		public static void main(String[]args)throws InterruptedException {
+		{  
+	    
+		StackItUp rect=new StackItUp(); 
 			 while(true)
 			 {
 				rect.move();
 				rect.repaint();
 				Thread.sleep(15);	 
+
 			 }
-		}
-	
-	public static void PlaySound(File Sound)
-	{
-		try{
-			Clip clip = AudioSystem.getClip();
-			clip.open(AudioSystem.getAudioInputStream(Sound));
-			clip.start();
-			
-			Thread.sleep(clip.getMicrosecondLength()/1000);
-			
-		}catch(Exception e)
-		{
-			
 		}
 	}
 }
-
 @SuppressWarnings("serial")
  class Block extends JFrame{
 	double vel=2.0;
-	int x=0,w=250,h=50,y=600,r=255,gn=255,b=255;
+	int x,w,h=50,y=650,r=255,gn=255,b=255;
+
 	 Block(){
 		 int r = (int)(Math.random()*((255+1)));
 		 int gn = (int)(Math.random()*((255+1)));
 		 int b = (int)(Math.random()*((255+1)));
-		//double vel=2.0;
 		this.x=x;
 		this.y=y;
 		this.w=w;
 		this.h= h;
-		this.vel=2.0;
+		this.vel=vel;
 		this.r=r;
 		this.gn=gn;
 		this.b=b;
@@ -132,12 +167,10 @@ class StackItUp extends JFrame{
 	@Override
 	public void paint(Graphics g)
 	{
-	 g.setColor(new Color(r, gn, b)); //apply random value using rand()
-		//g.setColor(Color.RED);
+
+	 g.setColor(new Color(r, gn, b));
+
 	 g.fillRect(x,y,w,h);
-	}
-	void drop() {
-		
 	}
 	public void keyPressed(KeyEvent e) {
 		if (e.getKeyCode() == KeyEvent.VK_DOWN || e.getKeyCode() == KeyEvent.VK_ENTER)
